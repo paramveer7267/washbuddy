aee0b27618799c3b96d2b03bbde80e8553976d04
Paramveer Singh <paramveer7267@gmail.com>
2025-11-28 16:54:09 +0530

    landpage working

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import { JSX } from "react";
 import Footer from "@/components/Footer";
-export default function RootLayout({ children }: { children: JSX.Element }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
